d10dde35ef2d91dfb481a94e087be4a8ba93c585
Brennan Gambling <brennan.gambling@gmail.com>
2017-12-02 14:43:22 -0500

    Finished the LinkedList class.

--- src/com/brennangambling/LinkedList.java
+++ src/com/brennangambling/LinkedList.java
@@ -1,4 +1,55 @@
 package com.brennangambling;
 
 public class LinkedList {
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public LinkedList() {
+        size = 0;
+    }
+
+    public String get(int index){
+        return get(index, first).string;
+    }
+
+    private Node get(int index, Node node) {
+        if (node != null) {
+            if (index == 0) {
+                return node;
+            } else {
+                return get(index - 1, node.next);
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public void add(String s) {
+        Node newNode = new Node(s);
+        if (first == null) {
+            first = newNode;
+        } else {
+            add(newNode, first);
+        }
+        size++;
+    }
+
+    private void add(Node newNode, Node currentNode) {
+        if (currentNode.next == null) {
+            currentNode.next = newNode;
+        } else {
+            add(newNode, currentNode.next);
+        }
+    }
+
+    public class Node {
+        public String string;
+        public Node next;
+
+        public Node(String s) {
+            string = s;
+        }
+    }
 }
